06f837a2700e4cfdb3d663971be19d30a61ca06d
venkykatta <venkateshkatta9705@gmail.com>
2021-06-20 15:45:22 +0530

    ajax concept in python

---
+++ src/posts/static/posts/main.js
@@ -0,0 +1,16 @@
+// console.log('Hello World');
+
+const helloWorldBox = document.getElementById('hello-world')
+
+$.ajax({
+    type : 'GET',
+    url : 'hello-world/',
+    success : function(response){
+        console.log('success', response.text)
+            helloWorldBox.textContent = response.text
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
+
